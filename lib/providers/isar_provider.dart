ded61221a863e3af2c2ec0cb54346ad408ee9074
fanawa <fmff59@gmail.com>
2024-08-09 10:50:57 +0900

    記述もれ修正

--- lib/providers/isar_provider.dart
+++ lib/providers/isar_provider.dart
@@ -6,13 +6,11 @@ import 'package:path_provider/path_provider.dart';
 import 'package:peeker/model/isar/isar_model.dart';
 
 Future<Isar> isarProvider({String? dirPath}) async {
-  // TODO(a): 生体認証時時にdeviceIdを登録
   const FlutterSecureStorage storage = FlutterSecureStorage();
   final String? deviceId = await storage.read(key: 'deviceId');
   final String instanceName = 'PEEKER_$deviceId';
 
-  // TODO(a): deiceIdもデータベース名に追加する  ex: 'IDz_$deviceId'
-  Isar? isar = Isar.getInstance('IDz_');
+  Isar? isar = Isar.getInstance('PEEKER_');
 
   if (isar != null) {
     return isar;
